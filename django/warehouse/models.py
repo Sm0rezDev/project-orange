513ac8956b46eefbad82260662d8dabd6242c8d9
Sm0rezDev <simonw0210@gmail.com>
2025-06-13 02:17:31 +0200

    Database design

--- django/warehouse/models.py
+++ django/warehouse/models.py
@@ -2,14 +2,40 @@ from django.db import models
 
 # Create your models here.
 
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    quantity = models.IntegerField()
+class Branch(models.Model):
+    BranchID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100, default='Rosersberg')
 
-class ProductBatch(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    ProductionDate = models.DateField()
-    ExpiryDate = models.DateField()
-    lot_number = models.IntegerField
-    batch_number = models.CharField(max_length=50)
-    quantity = models.IntegerField()
+class Orders(models.Model):
+    OrderID = models.AutoField(primary_key=True)
+    Created = models.DateTimeField(auto_now_add=True)
+    Status = models.CharField(max_length=50, choices=[
+        ('CREATED', 'Created'),
+        ('PROCESSING', 'Processing'),
+        ('COMPLETED', 'Completed')
+    ])
+    Branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+
+class Products(models.Model):
+    ProductID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100)
+    Group = models.CharField(max_length=3)
+
+class Production(models.Model):
+    ProductionID = models.AutoField(primary_key=True)
+    Product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    Batch = models.CharField(max_length=50, default='0000')
+    Date = models.DateField(auto_now_add=True)
+
+class Packing(models.Model):
+    Product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    Production = models.ForeignKey(Production, on_delete=models.CASCADE)
+    Lot = models.CharField(max_length=50, default='0000')
+    Quantity = models.IntegerField(default=0)
+    Date = models.DateField(auto_now_add=True)
+    BestBefore = models.DateField(auto_now_add=True)
+
+class OrderProduct(models.Model):
+    Order = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    Product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    Quantity = models.IntegerField(default=0)
