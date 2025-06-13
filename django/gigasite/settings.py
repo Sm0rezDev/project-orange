eb94335cf82206f044ad80b92639cbfd2b84f60a
Sm0rezDev <simonw0210@gmail.com>
2025-06-13 02:22:40 +0200

    saves

--- django/gigasite/settings.py
+++ django/gigasite/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # for development
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
