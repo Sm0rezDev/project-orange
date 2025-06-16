4984996bfb9fef97143daea4b20b942c1cf8a736
Sm0rezDev <simonw0210@gmail.com>
2025-06-16 02:33:33 +0200

    default db URL for testing

--- django/gigasite/settings.py
+++ django/gigasite/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'gigasite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=False)
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL', 'postgres://user:password@db:5432/test_db'), conn_max_age=600, ssl_require=False)
 }
 
 
