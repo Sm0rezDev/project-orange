8df39a4affc34555ca65ecce51d26ba765db59e5
Sm0rezDev <simonw0210@gmail.com>
2025-06-21 14:08:08 +0200

    paths

--- django/warehouse/urls.py
+++ django/warehouse/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("status/", views.inventory),
-    path("packing/", views.packing),
+    path("", views.get_csrf_token),
+    path("status/", views.status),
+    path("production/new/", views.add_production),
+    path("packing/", views.add_packing),
     ]
