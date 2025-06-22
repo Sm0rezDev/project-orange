3b4d1733a167be1cacabdd26efc88cfb157c38f1
Sm0rezDev <simonw0210@gmail.com>
2025-06-23 01:15:20 +0200

    type list instance

--- django/gigasite/settings.py
+++ django/gigasite/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1, localhost')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
-if ALLOWED_HOSTS:
+if isinstance(ALLOWED_HOSTS, str):
     ALLOWED_HOSTS = str(ALLOWED_HOSTS).replace(' ', '').split(',')
 
 CSRF_COOKIE_SECURE = str(os.getenv('CSRF_COOKIE_SECURE', 'True')) == 'True'
